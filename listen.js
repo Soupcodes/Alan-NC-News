f9b5c868ab43362fe7d6be28875ccf39ebfb08b8
Soupcodes <alantong789@gmail.com>
2019-07-19 08:45:25 +0100

    refactored file

--- listen.js
+++ listen.js
@@ -5,11 +5,3 @@ const { PORT = 9090 } = process.env;
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
 });
-
-// app.listen(process.env.PORT || 9090, () => {
-//   console.log(
-//     "Express server listening on port %d in %s mode",
-//     this.address().port,
-//     app.settings.env
-//   );
-// });
